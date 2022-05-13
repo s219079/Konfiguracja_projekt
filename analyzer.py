4ba94a1c5fbe59a97c2a261f841e0b95a3de7207
s219079 <92718129+s219079@users.noreply.github.com>
2022-05-13 11:22:13 +0200

    2022 05 13 HK

--- analyzer.py
+++ analyzer.py
@@ -5,4 +5,19 @@ print(*[filename.split(".")[0] for filename in os.listdir("./opinions")], sep = 
 product_id = input("Wprowadz identyfikator produktu: ")
 
 opinions = pd.read_json(f"opinions/{product_id}.json")
-print(opinions)
+
+opinions_count = len(opinions.index)
+
+pros_count = opinions.pros.map(bool).sum()
+cons_count = opinions.cons.map(bool).sum()
+average_score = opinions.stars.mean().round(2)
+
+recommendation = opinions.recommendation.value_counts(dropna = False)
+print(recommendation)
+recommendation.plot.pie(label="", autopct="%1.1", colors["forestgreen", "lightskublue", "crimson"])
+
+plt.title("Rekomendacja")
+plt.show()
+plt.grid(True)
+plt.close()
+print(pros_count)
